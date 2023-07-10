b30ab076d02631b0514231306cc92cb46fcb45e3
jcmunav63 <jcmunav63@gmail.com>
2023-07-10 14:51:09 -0600

    Edit index.html & main.js to insert book in local storage.

--- main.js
+++ main.js
@@ -1,21 +1,46 @@
-books =[];
+books = [{bookTitle: 'The Lord of the Rings', authorName: 'XXX'},
+{bookTitle: 'The Little Prince', authorName: 'YYY'}];
 
 
 function display(){
-const displayedBooks =document.getElementById('displayedBooks');
-const div= document.getElementById('bookSection');
+  const displayedBooks = document.getElementById('displayedBooks');
+  const div= document.querySelectorAll('.book');
+  books.forEach((book) => {
+    div.innerHtml=`
+    <h3>${bookTitle}</h3>
+    <h3>${authorName}</h3>
+    <button class='removeBtn'>Remove</button>
+    `;
+    displayedBooks.appendChild(div);
+  });
+}
 
-
-div.innerHtml=`
-<h3>${bookTitle}</h3>
-<h3>${authorName}</h3>
-<button class='removeBtn'>Remove</button>
-`;
-displayedBooks.appendChild(div);}
+function addBook(title, author) {
+  const newBook = {title, author};
+  books.push(newBook);
+  localStorage.setItem('books', JSON.stringify(books));
+  
+};
 
 const addBtn=document.getElementById('addBtn');
-addBtn.addEventListener('click',()=>{
-  const bookTitle=document.getElementById('title').value;
-const authorName=document.getElementById('author').value;
-display(bookTitle,authorName);
-})
+  addBtn.addEventListener('click', () =>{
+    const bookTitle = document.getElementById('title').value;
+    const authorName = document.getElementById('author').value;
+    addBook(bookTitle, authorName);
+    console.log('click');
+  });
+
+
+  display();
+  addBtn.addEventListener('click', () =>{
+    const bookTitle = document.getElementById('title').value;
+    const authorName = document.getElementById('author').value;
+    addBook(bookTitle, authorName);
+    console.log('click');
+  });
+
+addEventListener('onload', () => {
+  
+  display();
+///  console.log('onload');
+ });

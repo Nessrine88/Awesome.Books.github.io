51e99eff5ec6d7ac9390bf1359ae989fec3fe134
Nessrine88 <macherki.nessrine@yahoo.com>
2023-07-13 20:55:54 +0100

    add event listner to the navbar links

--- main.js
+++ main.js
@@ -1,5 +1,30 @@
 /* eslint-disable max-classes-per-file */
 /* eslint-disable no-use-before-define */
+
+const list = document.getElementById('list');
+const addBooks = document.getElementById('addBooks');
+const contact = document.getElementById('contact');
+
+const heading = document.getElementById('heading');
+const section2 = document.getElementById('section2');
+const section3 = document.getElementById('section3');
+const section4 = document.getElementById('section4');
+
+list.addEventListener('click', () => {
+  section2.style.display = 'none';
+  section3.style.display = 'block';
+  section4.style.display = 'none';
+  heading.style.display = 'block';
+  console.log('click');
+});
+
+
+list.addEventListener('click',()=>{
+  // section3.style.display='none'
+  section2.style.display='none';
+heading.style.display='block';
+console.log('click');
+})
 class Book {
   constructor(title, author) {
     this.title = title;
@@ -70,3 +95,16 @@ class BookCollection {
 
 // eslint-disable-next-line no-unused-vars
 const bookCollection = new BookCollection();
+
+
+function updateDateTime() {
+  const currentDateTime = new Date();
+  const datetimeElement = document.getElementById('datetime');
+  datetimeElement.textContent = currentDateTime.toLocaleString();}
+
+setInterval(updateDateTime, 1000);
+
+// Initial update
+updateDateTime();
+
+
